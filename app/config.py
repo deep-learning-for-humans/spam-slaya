a03db2291db76ef3e27938169bc9bdaa3f1cb3e5
Shrayas <shrayasr@gmail.com>
2024-10-11 07:59:53 +0530

    Prefix all env with SPAM_SLAYA_ for disambiguation

--- app/config.py
+++ app/config.py
@@ -5,13 +5,13 @@ class Config:
 
     load_dotenv()
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get("SPAM_SLAYA_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SPAM_SLAYA_DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    RQ_BROKER_URL = os.environ.get('RQ_BROKER_URL')
+    RQ_BROKER_URL = os.environ.get("SPAM_SLAYA_RQ_BROKER_URL")
 
-    CLIENT_SECRET_PATH = os.environ.get('GOOGLE_CLIENT_SECRET_PATH')
+    CLIENT_SECRET_PATH = os.environ.get("SPAM_SLAYA_GOOGLE_CLIENT_SECRET_PATH")
 
-    OLLAMA_URL = os.environ.get("OLLAMA_URL")
-    OLLAMA_MODEL = os.environ.get("OLLAMA_MODEL")
+    OLLAMA_URL = os.environ.get("SPAM_SLAYA_OLLAMA_URL")
+    OLLAMA_MODEL = os.environ.get("SPAM_SLAYA_OLLAMA_MODEL")
