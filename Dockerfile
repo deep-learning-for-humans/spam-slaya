cf14339e51e186253ac65fc98dc0864479e481ee
Shrayas <shrayasr@gmail.com>
2024-10-22 23:04:22 +0530

    Remove all files to ready for landing page

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM python:3.12-alpine
-WORKDIR /app
-COPY . /app
-RUN pip install -r requirements.txt
--- run.py
+++
@@ -1,6 +0,0 @@
-from app import create_app, db
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run("0.0.0.0", 8080, debug=True)
